61ed2ab17ebdaee2f417c0012ed5522297b61c02
sofiehilge <sofie.hilge@gmail.com>
2023-02-08 13:59:19 +0100

    add template litteral solution

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let heading = document.querySelector(".heading")
 
 heading.textContent = ministre.partiforhold.socialdemokratiet.partinavn + " " + ministre.partiforhold.socialdemokratiet.oprettet
 
-//----------vis moderaterne og årstal i paragraf
+//----------vis moderaterne og årstal i paragraf med template litterals.
+let para = document.querySelector(".p")
 
-
+para.textContent = `
+${ministre.partiforhold.venstre.Partinavn}
+${ministre.partiforhold.venstre.Oprettet}
+`
