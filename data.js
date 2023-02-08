eaf43b9792ae3ef483599db6b4cd7e30f3bd00b1
sofiehilge <sofie.hilge@gmail.com>
2023-02-08 13:26:08 +0100

    add data js

--- data.js
+++ data.js
@@ -45,8 +45,8 @@
 //body.append(liste);
 
 let ministre = {
-    partiforhold: [{// hvis vi laver et overordnet objekt giver det os mulighed for at udvide med andre kategorier
-        Socialdemokratiet:[
+    partiforhold: {// hvis vi laver et overordnet objekt giver det os mulighed for at udvide med andre kategorier
+        Socialdemokratiet:
             {// der er lavet et objekt rundt om for at kunne lave nogle properties som kategorisere arrayet
             Mand: [{name: "Nicolai Wammen", ministerpost: "Finansminister", age: 52, married: true, kids:["dreng","dreng","dreng"], birthdate: "7.11.1971", education: "Cand.scient.pol"}, 
                     {name: "Morten Bødskov", ministerpost: "Erhvervsminister", age: 53, married: undefined, kids: undefined, birthdate: "01.05.1970", education: "BA i samfundsfag"}, 
@@ -55,16 +55,14 @@ let ministre = {
                     {name: "Mattias Tesfaye", ministerpost: "Børne- og undervisningsminister", age: 42, married: true, kids: true, birthdate: "31.03.1981", education: "muresvend"},
                     {name: "Peter Hummelgaard Thomsen", ministerpost:"Justitsminister", age: 40, married: undefined, kids: undefined, birthdate: "17.01.1983", education:"Cand.jur"},
                     {name: "Kaare Dybvad Bek", ministerpost: "Udlændinge- og integrationsminister", age: 39, married: undefined, kids: undefined, birthdate: "05.08.1984", education:"Cand.scient"},
-                    {name: "Jeppe Bruus Christensen", ministerpost: "Skatteminister", age: 45, married: true, kids: ["pige", "pige"], birthdate: "20.04.1978", education: "Cand.scient.pol"},  
-                ],
-
+                    {name: "Jeppe Bruus Christensen", ministerpost: "Skatteminister", age: 45, married: true, kids: ["pige", "pige"], birthdate: "20.04.1978", education: "Cand.scient.pol"},
+            ],
             Kvinde:[{name: "Mette Frederiksen", ministerpost: "Statsminister", age: 46, married: true, kids:["dreng", "pige"], birthdate: "19.11.1977", education: "Ma i Afrikastudier"}, 
                     {name: "Pernille Rosenkrantz-Theil" , ministerpost: "Social- og boligminister", age: 46, married: undefined, kids: undefined, birthdate:"17.01.1977", education: "Ba Statskundskab" }, 
-                    {name: "Ane Halsboe-Jørgensen", ministerpost: "beskæftigelsesminister", age: 40, married: undefined, kids: undefined, birthdate:"04.05.1983", education:"Cand.scient.pol"}, 
-                    ]
-        },
-        ],
-        Moderaterne:[
+                    {name: "Ane Halsboe-Jørgensen", ministerpost: "beskæftigelsesminister", age: 40, married: undefined, kids: undefined, birthdate:"04.05.1983", education:"Cand.scient.pol"}
+            ],
+                },
+        Moderaterne:
             {// der er lavet et objekt rundt om for at kunne lave nogle properties som kategorisere arrayet
             Mand: [{name: "Lars Løkke Rasmussen", ministerpost: "Udenrigsminister", age: 59, married: true, kids: undefined, birthdate: "15.05.1964", education: "Cand.jur"}, // Null indicates the absence of a value for a variable. Undefined indicates the absence of the variable itself.
                     {name: "Jakob Engel-Schmidt", ministerpost: "moderakulturminister", age: 37, married: undefined, kids: undefined, birthdate: "24.oktober.1983", education: "Cand.merc"},
@@ -75,8 +73,7 @@ let ministre = {
 
         ]
         },
-        ],
-        Venstre:[
+        Venstre:
             {// der er lavet et objekt rundt om for at kunne lave nogle properties som kategorisere arrayet
              Mand: [{name: "Jakob Elleman-Jensen", ministerpost: "Forsvarsminister", age: 50, married: true, kids: true, birthdate: "25.09.1973", education:"cand.merc.jur"},
                     {name:"Troels Lund Poulsen", ministerpost: "Økonomiminister", age: 47, married: undefined, kids: undefined, birthdate: "30.03.1976", education:"Stud.gym" },
@@ -89,9 +86,7 @@ let ministre = {
                 {name:"Marie Bjerre", ministerpost: "Digitaliseringsminister", age: 36, married: true, kids:["pige", "pige"], birthdate:"06.05.1986", education:"cand.jur"},
             ]
             },
-        ]
     },
-],
     
 
 }
